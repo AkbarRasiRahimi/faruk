dd4e5c3a60cdf103975e86f382fd42bf7246ebd3
Akbar RasiRahimi <akbar.rasirahimi.1980@gmail.com>
2024-06-11 08:52:12 +0300

    New

--- src/app/(intern)/(application)/create-application/page.js
+++ src/app/(intern)/(application)/create-application/page.js
@@ -206,16 +206,3 @@ export default function UserApplications() {
   );
 }
 
-// import React from "react";
-
-// const Loading = () => {
-//   return (
-
-//     <div className="flex h-full justify-center items-center">
-//       <span className="loading loading-ring w-56  h-56"></span>
-//       <div className="text-3xl text-white">Create Application</div>
-//     </div>
-//   );
-// };
-
-// export default Loading;
